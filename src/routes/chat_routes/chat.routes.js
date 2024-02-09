6027b1cf4c94700727fb0a3303e25496b3be6045
developermactosys <developermactosys@gmail.com>
2024-02-09 13:24:16 +0000

    ok

--- src/routes/chat_routes/chat.routes.js
+++ src/routes/chat_routes/chat.routes.js
@@ -6,11 +6,12 @@ const {updateChatStatus } = require("../../controller/chat_controller/chat_statu
 const {getChatStatus } = require("../../controller/chat_controller/get_chat_status.controller")
 const { sendUserChatRequest} = require("../../controller/chat_controller/send_user_chat_request.controller")
 const { rejectUserChatRequest} = require("../../controller/chat_controller/reject_user_chat_request.controller")
-
+const {getChatList_by_user_id} = require("../../controller/chat_controller/chat_list.controller")
 
 router.post('/send_user_chat_request/:request_id',uploads.none(), sendUserChatRequest);// Done 
 router.post('/reject_user_chat_request',uploads.none(), rejectUserChatRequest);// Done
 router.get('/chat_active_status',uploads.none(),getChatStatus);
 router.post('/chat_status_update',uploads.none(), updateChatStatus);
+router.post('/get_chat_list_by_user_id',uploads.none(), getChatList_by_user_id);
 
 module.exports = router;
