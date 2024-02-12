3dd98a9f1f83acaa6c7c57555e5e69e5c894cebd
akash <akash.bawankar111@gmail.com>
2024-02-12 18:17:49 +0530

    changes on chatlist

--- src/controller/chat_controller/chat_list.controller.js
+++ src/controller/chat_controller/chat_list.controller.js
@@ -8,28 +8,45 @@ const sequelize = require('sequelize');
 exports.getChatList_by_user_id = async (req, res) => {
   try {
     const { user_id } = req.query;
-    const chatList = await Chat.findAll({
-      where: {
-        [sequelize.Op.or]: [
-          { sender_id: user_id },
-          { receiver_id: user_id }
-        ]
-      },
+    const chatList_as_sender = await Chat.findAll({
+      where:    
+          { sender_id: user_id }
+      ,
       attributes: ['id', 'sent_date', 'sent_time', 'message'],
       include: [
         { model: User, 
             as: 'User', 
             attributes: ['id', 'name', 'profile_image'] 
         },
+       
+      ],
+      group: [ 'sender_id'],
+      order: [['sent_date', 'DESC']]
+    });
+
+    const chatList_as_receiver = await Chat.findAll({
+      where:    
+          { receiver_id: user_id }
+      ,
+      attributes: ['id', 'sent_date', 'sent_time', 'message'],
+      include: [
         { model: User, 
             as: 'User', 
             attributes: ['id', 'name', 'profile_image'] 
-        }
+        },
+       
       ],
-      group: ['receiver_id', 'sender_id'],
+      group: [ 'receiver_id'],
       order: [['sent_date', 'DESC']]
     });
-    res.json({ data: chatList, status: true, message: "Chat List retrieved successfully" });
+
+    res.json({
+      status: true, 
+      message: "Chat List retrieved successfully" ,
+      Chatdata_as_receiver: chatList_as_receiver,
+      Chatdata_as_sender: chatList_as_sender
+      });
+      
   } catch (error) {
     console.error(error);
     res.status(500).json({ status: false, message: "Internal server error" });
