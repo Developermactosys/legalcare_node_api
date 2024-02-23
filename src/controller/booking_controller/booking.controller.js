b116a1027be45a2b96a72e31cc2a3175a7cceae7
akash <akash.bawankar111@gmail.com>
2024-02-23 18:35:23 +0530

    booking api's correction's

--- src/controller/booking_controller/booking.controller.js
+++ src/controller/booking_controller/booking.controller.js
@@ -1,11 +1,12 @@
 // const { where } = require("sequelize");
-const { where } = require("sequelize");
+const { where,Sequelize } = require("sequelize");
 const db = require("../../../config/db.config");
 const Booking_details = db.booking_detail;
 const service = db.service;
+const User = db.User
 exports.Add_Booking = async (req, res) => {
   try {
-    const { serviceId, discounted_amount, GST,user_id } = req.body;
+    const { serviceId, discounted_amount, GST, user_id } = req.body;
 
     const isEmptykey = Object.keys(req.body).some((key) => {
       const value = req.body[key];
@@ -36,7 +37,7 @@ exports.Add_Booking = async (req, res) => {
 
 exports.get_booking_by_status = async (req, res) => {
   try {
-    const { status ,user_id} = req.body;
+    const { status, user_id } = req.body;
     const isEmptykey = Object.keys(req.body).some((key) => {
       const value = req.body[key];
       return value === "" || value === null || value === undefined;
@@ -49,12 +50,22 @@ exports.get_booking_by_status = async (req, res) => {
       const pending_booking = await Booking_details.findAll({
         where: {
           status: status,
-          UserId:user_id,
-        }, include: [{
-          model: service,
-          as: "service",
-          // attributes: ['service_img', 'service_cost','serviceName','id'],
-      }]
+          UserId: user_id,
+        },
+        include: [
+          {
+            model: service,
+            as: "service",
+            include: [
+              {
+                model: User,
+                as: "User",
+                where: { id: Sequelize.col('service.UserId') } // Here, we specify the association between the User model and the service model using the UserId from the service object
+              }
+            ]
+          }
+        ],
+        order: [['createdAt', 'DESC']],
       });
 
       return res.status(200).json({
@@ -65,42 +76,63 @@ exports.get_booking_by_status = async (req, res) => {
     }
 
     if (status === "reject") {
-        const rejected_booking = await Booking_details.findAll({
-          where: {
-            status: status,
-            UserId:user_id,
-          },include: [{
+      const rejected_booking = await Booking_details.findAll({
+        where: {
+          status: status,
+          UserId: user_id,
+        },
+        include: [
+          {
             model: service,
             as: "service",
-            // attributes: ['service_img', 'service_cost','serviceName','id'],
-        }]
-        });
-  
-        return res.status(200).json({
-          status: true,
-          message: "rejected bookings",
-          data: rejected_booking,
-        });
+            include: [
+              {
+                model: User,
+                as: "User",
+                where: { id: Sequelize.col('service.UserId') } // Here, we specify the association between the User model and the service model using the UserId from the service object
+              }
+            ]
+          }
+        ],
+        order: [['createdAt', 'DESC']],
+      });
+
+      return res.status(200).json({
+        status: true,
+        message: "rejected bookings",
+        data: rejected_booking,
+      });
     }
 
     if (status === "approved") {
-        const approved_booking = await Booking_details.findAll({
-          where: {
-            status: status,
-            UserId:user_id,
-          },include: [{
+      const approved_booking = await Booking_details.findAll({
+        where: {
+          status: status,
+          UserId: user_id,
+        },
+        include: [
+          {
             model: service,
             as: "service",
-            // attributes: ['service_img', 'service_cost','serviceName','id'],
-        }]
-        });
-  
-        return res.status(200).json({
-          status: true,
-          message: "approved bookings",
-          data: approved_booking,
-        });
-      }
+            include: [
+              {
+                model: User,
+                as: "User",
+                where: { id: Sequelize.col('service.UserId') } // Here, we specify the association between the User model and the service model using the UserId from the service object
+              }
+            ]
+          }
+        ],
+        order: [['createdAt', 'DESC']],
+
+      });
+
+      return res.status(200).json({
+        status: true,
+        message: "approved bookings",
+        data: approved_booking,
+      });
+    }
 
   } catch (error) {
     console.error(error);
@@ -113,92 +145,109 @@ exports.get_booking_by_status = async (req, res) => {
 
 
 // get all Bookings 
-exports.getAll_bookings = async(req, res) => {
-    try {
-      
-        const get_all_booking = await Booking_details.findAll({
-          include: [{
+exports.getAll_bookings = async (req, res) => {
+  try {
+
+    const get_all_booking = await Booking_details.findAll({
+      include: [
+        {
           model: service,
           as: "service",
-          // attributes: ['service_img', 'service_cost','serviceName','id'],
-      }]})
+          include: [
+            {
+              model: User,
+              as: "User",
+              where: { id: Sequelize.col('service.UserId') } // Here, we specify the association between the User model and the service model using the UserId from the service object
+            }
+          ]
+        }
+      ],
+      order: [['createdAt', 'DESC']],
+    })
 
-        return res.status(200).json({
-            status : true,
-            message : "All Booking",
-            data : get_all_booking
-        })
-        
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-          status: false,
-          message: error.message,
-        });
-    }
+    return res.status(200).json({
+      status: true,
+      message: "All Booking",
+      data: get_all_booking
+    })
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      status: false,
+      message: error.message,
+    });
+  }
 }
 
 // get Bookings by id 
-exports.get_bookings_by_user_id= async(req, res) => {
+
+exports.get_bookings_by_user_id = async (req, res) => {
   try {
     const { user_id } = req.query;
     const page = parseInt(req.query.page) || 1;
     const pageSize = parseInt(req.query.pageSize) || 5;
     const offset = (page - 1) * pageSize;
-      const get_booking = await Booking_details.findAll({
-        where : {UserId : user_id},
-        include: [{
-        model: service,
-        as: "service",
-        // attributes: ['service_img', 'service_cost','serviceName','id'],
-    }],
-    order:[['createdAt','DESC']],
-    offset:offset,
-    limit:pageSize,
-  })
+    const get_booking = await Booking_details.findAll({
+      where: { UserId: user_id },
+      include: [
+        {
+          model: service,
+          as: "service",
+          include: [
+            {
+              model: User,
+              as: "User",
+              where: { id: Sequelize.col('service.UserId') } // Here, we specify the association between the User model and the service model using the UserId from the service object
+            }
+          ]
+        }
+      ],
+      order: [['createdAt', 'DESC']],
+      offset: offset,
+      limit: pageSize,
+    })
 
+    return res.status(200).json({
+      status: true,
+      message: "All Booking",
+      data: get_booking,
+      currentPages: page,
+    })
 
-      return res.status(200).json({
-          status : true,
-          message : "All Booking",
-          data : get_booking,
-          currentPages:page,
-
-      })
-      
   } catch (error) {
-      console.error(error);
-      return res.status(500).json({
-        status: false,
-        message: error.message,
-      });
+    console.error(error);
+    return res.status(500).json({
+      status: false,
+      message: error.message,
+    });
   }
 }
 
 
 
 // Cancle Booking 
-exports.Cancle_booking_by_id = async(req, res) => {
-    try {
-        const {booking_id}  = req.params
-        const cancel_booking = await Booking_details.findByPk(booking_id)
-        if(cancel_booking){
-       await cancel_booking.destroy(cancel_booking)
-        return res.status(200).json({
-            status : true,
-            message : "Booking delete Successfully"
-        })
-    }else{
-        return res.status(400).json({
-            status : false,
-            message : "Booking Id not found or Booking not deleted"
-        })
+exports.Cancle_booking_by_id = async (req, res) => {
+  try {
+    const { booking_id } = req.params
+    const cancel_booking = await Booking_details.findByPk(booking_id)
+    if (cancel_booking) {
+      await cancel_booking.destroy(cancel_booking)
+      return res.status(200).json({
+        status: true,
+        message: "Booking delete Successfully"
+      })
+    } else {
+      return res.status(400).json({
+        status: false,
+        message: "Booking Id not found or Booking not deleted"
+      })
     }
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-          status: false,
-          message: error.message,
-        });
-    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      status: false,
+      message: error.message,
+    });
+  }
 }
