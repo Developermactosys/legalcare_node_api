bee46dcc1f11dab8f9f8235c79d4053019cb758d
akash <akash.bawankar111@gmail.com>
2024-02-27 18:54:28 +0530

    get_user_list

--- src/controller/chat_controller/chat_list.controller.js
+++ src/controller/chat_controller/chat_list.controller.js
@@ -1,43 +1,36 @@
 
-// // controllers/chatController.js
+
 const db = require("../../../config/db.config");
 const User = db.User;
 const Chat = db.chat;
-const { Op } = require('sequelize'); // Import Op for Sequelize operators
+const { Op,Sequelize  } = require('sequelize'); // Import Op for Sequelize operators
+
 
 exports.getChatList_by_user_id = async (req, res) => {
   try {
-    const { user_id } = req.query;
-
-    // Find all chats where the user is the sender
-    const chatList_as_sender = await Chat.findAll({
-      where: { sender_id: user_id },
-      attributes: ['id', 'sent_date', 'sent_time', 'message'],
-      // include: [{
-      //   model: User,
-      //   as: 'User', // Use alias for clarity
-      //   attributes: ['id', 'name', 'profile_image']
-      // }],
-      order: [['sent_date', 'DESC']]
-    });
-
-    // Find all chats where the user is the receiver
-    const chatList_as_receiver = await Chat.findAll({
-      where: { receiver_id: user_id },
-      attributes: ['id', 'sent_date', 'sent_time', 'message'],
-      // include: [{
-      //   model: User,
-      //   as: 'User', // Use alias for clarity
-      //   attributes: ['id', 'name', 'profile_image']
-      // }],
-      order: [['sent_date', 'DESC']]
+    const { sender_id,receiver_id } = req.query;
+if(!sender_id && !receiver_id ){
+  return res.status(400).json({
+    status:false,
+    msg:"Please provide your sender_id and receiver_id"
+  })
+}
+    // Find all chats where the user is either the sender or the receiver
+    const chatList = await Chat.findAll({
+      where: {
+        [Op.and]: [
+          { sender_id: sender_id },
+          { receiver_id: receiver_id }
+        ]
+      },
+      // attributes: [ 'message','sent_date', 'sent_time' ],
+      order: [['id', 'DESC']]
     });
 
     res.json({
       status: true,
       message: "Chat List retrieved successfully",
-      Chatdata_as_sender: chatList_as_sender,
-      Chatdata_as_receiver: chatList_as_receiver,
+      chatList: chatList
     });
   } catch (error) {
     console.error(error);
@@ -46,4 +39,35 @@ exports.getChatList_by_user_id = async (req, res) => {
 };
 
 
+exports.getUserList_by_user_id = async (req, res) => {
+  try {
+    const { user_id } = req.query;
+    if(!user_id ){
+      return res.status(400).json({
+        status:false,
+        msg:"Please provide your user_id"
+      })
+    }
+    // Find all distinct users with whom the specified user has had chats
+    const userList = await User.findAll({
+      where: {
+        [Op.or]: [
+          Sequelize.literal(`id IN (SELECT DISTINCT sender_id FROM chats WHERE receiver_id = ${user_id})`),
+          Sequelize.literal(`id IN (SELECT DISTINCT receiver_id FROM chats WHERE sender_id = ${user_id})`)
+        ]
+      },
+      attributes: ['id', 'name', 'profile_image'],
+      order: [['id','DESC']]
 
+    });
+
+    res.json({
+      status: true,
+      message: "User list retrieved successfully",
+      userList: userList
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: false, message: "Internal server error" });
+  }
+};
--- src/routes/chat_routes/chat.routes.js
+++ src/routes/chat_routes/chat.routes.js
@@ -6,7 +6,7 @@ const {updateChatStatus } = require("../../controller/chat_controller/chat_statu
 const {getChatStatus } = require("../../controller/chat_controller/get_chat_status.controller")
 const { sendUserChatRequest} = require("../../controller/chat_controller/send_user_chat_request.controller")
 const { rejectUserChatRequest} = require("../../controller/chat_controller/reject_user_chat_request.controller")
-const { getChatList_by_user_id } = require("../../controller/chat_controller/chat_list.controller");
+const { getChatList_by_user_id,getUserList_by_user_id } = require("../../controller/chat_controller/chat_list.controller");
 const { messageUser, getAllMessages } = require("../../controller/chat_controller/add_chat.controller")
 
 router.post('/send_user_chat_request/:request_id',uploads.none(), sendUserChatRequest);// Done 
@@ -17,5 +17,5 @@ router.post('/chat_status_update',uploads.none(), updateChatStatus);
 router.post('/get_chat_list_by_user_id',uploads.none(), getChatList_by_user_id);
 router.post('/add_chat_msg',uploads.none(), messageUser)
 router.get('/get_all_chat',uploads.none(), getAllMessages)
-
+router.get('/get_user_list_by_user_id',getUserList_by_user_id)
 module.exports = router;
