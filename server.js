800d07f416be2667f394e86abe93900f0b36c954
akash <akash.bawankar111@gmail.com>
2024-02-27 12:10:04 +0530

    socket.io added

--- server.js
+++ server.js
@@ -5,12 +5,21 @@ const multer = require("multer");
 const app = express();
 
 const path = require('path');
-const http = require('http');
-const { Server } = require('socket.io');
+// const { Server } = require('socket.io');
 const cookieParser = require('cookie-parser');
-const server = http.createServer(app);
-const io = new Server(server);
+// const server = http.createServer(app);
+// const io = new Server(server);
 const cors = require("cors");
+
+
+const http = require("http").Server(app);
+const socketIO = require("socket.io")(http, {
+
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 const corsOption = {
     credentials: true,
@@ -27,6 +36,20 @@ app.use(cookieParser());
 // app.use(multer().none());
 
 
+const handleUserData = require('./src/controller/socket_controller/socket.controller')
+const handleTimer = require('./src/controller/socket_controller/socket.controller')
+const handleUserStatusWeb = require('./src/controller/socket_controller/socket.controller')
+const handleRequestSending = require('./src/controller/socket_controller/socket.controller')
+const handleAcceptRequest = require('./src/controller/socket_controller/socket.controller')
+const handleAstroRequest = require('./src/controller/socket_controller/socket.controller')
+const handleChatHistory = require('./src/controller/socket_controller/socket.controller')
+const fetchChatHistory = require('./src/controller/socket_controller/socket.controller')
+const handleTyping = require('./src/controller/socket_controller/socket.controller')
+const handleForceDisconnect = require('./src/controller/socket_controller/socket.controller')
+const checkIsBusyHandler = require('./src/controller/socket_controller/socket.controller')
+const handleApproveWaitingStatus = require('./src/controller/socket_controller/socket.controller')
+const handleCallStatus = require('./src/controller/socket_controller/socket.controller')
+const handleDisconnect = require('./src/controller/socket_controller/socket.controller')
 
 // Routes
 const Routes = require('./src/routes/main.routes');
@@ -49,17 +72,45 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Socket.io
-io.on('connection', (socket) => {
-    socket.on('userMessage', (message) => {
-        // Consider saving the message to your database here
-        io.emit('message', message);
-    });
-});
+// io.on('connection', (socket) => {
+//     socket.on('userMessage', (message) => {
+//         // Consider saving the message to your database here
+//         io.emit('message', message);
+//     });
+// });
+// Socket.io
+socketIO.on('connection', (socket) => {
+    
+    handleUserData(socket, users, io);
+    handleTimer(socket, io);
+    handleUserStatusWeb(socket, io);
+    handleRequestSending(socket, io);
+    handleAcceptRequest(socket, io);
+    handleAstroRequest(socket, io);
+    handleChatHistory(socket);
+    fetchChatHistory(socket, data);
 
+    socket.on('typing', (data) => {
+        handleTyping(socket, data);
+    });
+
+    socket.on('force_disconnect', async (data) => {
+        await handleForceDisconnect(socket, data);
+    });
+
+    socket.on('check_is_busy', async (data) => {
+        await checkIsBusyHandler(socket, data);
+    });
+
+    socket.on("approve_waiting_status", (data) => handleApproveWaitingStatus(socket, io, User, data));
+    socket.on("call_status", (data) => handleCallStatus(socket, io, CallDetail, data));
+    socket.on('disconnect', () => handleDisconnect(socket, users));
+
+});
 
 
 // Start server
 const PORT = process.env.PORT || 7878;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+http.listen(PORT, () => {
+    console.log(`Server & Socket.io is running on port ${PORT}`);
 });
