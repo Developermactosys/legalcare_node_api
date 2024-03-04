f3898d1c66fa2baa23e82f6cccf56e20798e4343
Developermactosys <developermactosys@gmail.com>
2024-03-04 05:36:33 +0000

    shweta_changes

--- src/controller/agora_For_video/channel.controllers.js
+++ src/controller/agora_For_video/channel.controllers.js
@@ -4,9 +4,9 @@ const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const {RtcTokenBuilder, RtcRole} = require('agora-access-token');
-const APP_ID = process.env.AGORA_APP_ID;
-const APP_CERTIFICATE = process.env.AGORA_APP_CERTIFICATE; 
+const { RtcTokenBuilder, RtcRole } = require('agora-access-token');
+const APP_ID = 'f6568a063d6643289d64e0346940341e';
+const APP_CERTIFICATE = 'a3861d180f0a416b95fe563d7ea991ca';
 
 
 module.exports.join_channel_for_legalcare = (req, resp) => {
@@ -14,12 +14,12 @@ module.exports.join_channel_for_legalcare = (req, resp) => {
   // set response header
   resp.header('Access-Control-Allow-Origin', '*');
   const generatedchannelName = uuidv4();
-   // get uid
-   let uid = req.params.uid;
-   if(!uid || uid === '') {
-     return resp.status(400).json({ 'error': 'uid is required' });
-   }
- 
+  // get uid
+  let uid = req.params.uid;
+  if (!uid || uid === '') {
+    return resp.status(400).json({ 'error': 'uid is required' });
+  }
+
   // get role
   let role;
   if (req.params.role === 'publisher') {
@@ -34,7 +34,7 @@ module.exports.join_channel_for_legalcare = (req, resp) => {
   // get the expire time
   let expireTime = req.query.expiry;
   if (!expireTime || expireTime === '') {
-    expireTime = 60 * 60 ;
+    expireTime = 60 * 60;
   } else {
     expireTime = parseInt(expireTime, 10);
   }
@@ -48,17 +48,18 @@ module.exports.join_channel_for_legalcare = (req, resp) => {
   // token = RtcTokenBuilder.buildTokenWithAccount(APP_ID, APP_CERTIFICATE, channelName, role, privilegeExpireTime);
 
   if (req.params.tokentype === 'userAccount') {
-    token = RtcTokenBuilder.buildTokenWithAccount(APP_ID, APP_CERTIFICATE,generatedchannelName, uid, role, privilegeExpireTime);
+    token = RtcTokenBuilder.buildTokenWithAccount(APP_ID, APP_CERTIFICATE, generatedchannelName, uid, role, privilegeExpireTime);
   } else if (req.params.tokentype === 'uid') {
-    token = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE,generatedchannelName, uid, role, privilegeExpireTime);
+    token = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE, generatedchannelName, uid, role, privilegeExpireTime);
   } else {
     return resp.status(400).json({ 'error': 'token type is invalid' });
   }
 
   // return the token
-  return resp.json({ 'rtcToken': token,
-                    'generatedchannelName':generatedchannelName
- });
+  return resp.json({
+    'rtcToken': token,
+    'generatedchannelName': generatedchannelName
+  });
 }
 
 
