8bb61553c41f9e2a0ff3c16f6441dbf2f7b79452
akash <akash.bawankar111@gmail.com>
2024-03-07 11:14:17 +0530

    pagination = 10 get_allservice

--- src/controller/service_controller/service.controller.js
+++ src/controller/service_controller/service.controller.js
@@ -175,7 +175,7 @@ const getAllserviceBy_expert_id = async(req, res) => {
     try {
         const { expert_id } = req.query;
         const page = Number(req.query.page) || 1;
-        const limit = Number(req.query.limit) || 5;
+        const limit = Number(req.query.limit) || 10;
         const offset = (page - 1) * limit;
 
         const getServices = await services.findAll({where :{
