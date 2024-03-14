8ba3002a20d0230b44618f3038567ded296eefb4
akash <akash.bawankar111@gmail.com>
2024-03-14 16:57:11 +0530

    authorizaion and transaction_details

--- src/routes/admin_routes/banner_routes/banner.routes.js
+++ src/routes/admin_routes/banner_routes/banner.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {uploads} = require('../../../middleware/multer');
-const { authorize } = require("../../../middleware/authorization")
+// const { authorize } = require("../../../middleware/authorization")
 
 const {getAllBanner,addBanner,editBanner,deleteBanner,update_banner_status} = require("../../../controller/admin_controller/banner_controller/banner.controller");
 
@@ -11,9 +11,9 @@ const {getAllBanner,addBanner,editBanner,deleteBanner,update_banner_status} = re
 // router.delete("/delete_banner/:id", authorize(['0']),deleteBanner);
 // router.get("/getall_banner",authorize(['0']), getAllBanner);
 
-router.post("/add_banner",authorize(['0']), uploads.single("banner_image"), addBanner); // done
-router.patch("/edit_banner/:id", authorize(['0']),uploads.single("banner_image"), editBanner); // done
-router.delete("/delete_banner/:id",authorize(['0']), deleteBanner); // done
+router.post("/add_banner", uploads.single("banner_image"), addBanner); // done
+router.patch("/edit_banner/:id",uploads.single("banner_image"), editBanner); // done
+router.delete("/delete_banner/:id", deleteBanner); // done
 router.post("/get_banner_images", getAllBanner); //  done
 
 // For FrontEnd Team (admin_section)
