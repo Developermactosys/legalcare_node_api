acbf3538d8e105f0cad9a65bc5d3b68da93242c2
akash <akash.bawankar111@gmail.com>
2024-03-14 16:13:00 +0530

    authorizaion and transaction_details

--- src/routes/admin_routes/banner_routes/banner.routes.js
+++ src/routes/admin_routes/banner_routes/banner.routes.js
@@ -14,9 +14,9 @@ const {getAllBanner,addBanner,editBanner,deleteBanner,update_banner_status} = re
 router.post("/add_banner",authorize(['0']), uploads.single("banner_image"), addBanner); // done
 router.patch("/edit_banner/:id", authorize(['0']),uploads.single("banner_image"), editBanner); // done
 router.delete("/delete_banner/:id",authorize(['0']), deleteBanner); // done
-router.post("/get_banner_images",authorize(['0']), getAllBanner); //  done
+router.post("/get_banner_images", getAllBanner); //  done
 
 // For FrontEnd Team (admin_section)
-router.get("/get_banner_images",authorize(['0']), getAllBanner); //  done
-router.patch("/update_event_status",authorize(['0']),uploads.none(), update_banner_status);
+router.get("/get_banner_images", getAllBanner); //  done
+router.patch("/update_event_status",uploads.none(), update_banner_status);
 module.exports = router;
