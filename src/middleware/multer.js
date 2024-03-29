384031ab48a1895bbc95d3c81304e8983bc3ed9c
akash <akash.bawankar111@gmail.com>
2024-03-29 16:54:27 +0530

    ok

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -27,7 +27,17 @@ const fileStorage = multer.diskStorage({
     uploadPath = path.join(__dirname, "../../src/uploads/documents");
   } else if (file.fieldname === "document") {
     uploadPath = path.join(__dirname, "../../src/uploads/documents");
-  }else {
+  }
+  else if (file.fieldname === "pan_doc") {
+    uploadPath = path.join(__dirname, "../../src/uploads/documents");
+  }
+  else if (file.fieldname === "aadhar_doc") {
+    uploadPath = path.join(__dirname, "../../src/uploads/documents");
+  }
+  else if (file.fieldname === "passbook_img") {
+    uploadPath = path.join(__dirname, "../../src/uploads/documents");
+  }
+  else {
       console.log(`multer problem ${file.fieldname}`);
       return cb(new Error("Invalid fieldname"));
     }
--- src/routes/bank_routes/bank.routes.js
+++ src/routes/bank_routes/bank.routes.js
@@ -6,8 +6,9 @@ const {uploads} = require('../../middleware/multer');
 const { saveBankDetails ,getBankDetails,getBankDetailsById} = require("../../controller/bank_detail_controller/bank_detail.controller");
 
 
-router.post('/save_bank_details',uploads.none(),saveBankDetails); // Done 
+router.post('/save_bank_details',uploads.fields(['pan_doc','aadhar_doc', 'passbook_img']),saveBankDetails); // Done 
 router.get('/get_bank_details',getBankDetails)
-router.get('get_bank_detail_by_id/:id',getBankDetailsById);
+router.get('/get_bank_detail_by_id/:id',getBankDetailsById);
+
 
 module.exports = router;
