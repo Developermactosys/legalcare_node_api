64b2195bc65f00911b7f3faf9a8676a4c76c0d6c
Developermactosys <developermactosys@gmail.com>
2024-05-06 18:27:38 +0530

    ok

--- src/controller/admin_controller/customer_crud.controller.js
+++ src/controller/admin_controller/customer_crud.controller.js
@@ -398,6 +398,11 @@ exports.totalCountForCustomer = async(req, res)=>{
         where: {
           [Sequelize.Op.or]: [{ from_user_id: id }, { to_user_id: id }],
         },
+        include: [
+          model : User,
+          as : "User",
+          where : {deleted_At : null}
+        ]
         group: ["from_user_id", "to_user_id"],
       });
       let uniqueChatsCounts;
