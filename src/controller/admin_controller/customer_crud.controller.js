69b84e479635e86e0278deef486a887c9b6d7542
Developermactosys <developermactosys@gmail.com>
2024-05-06 18:59:44 +0530

    ok

--- src/controller/admin_controller/customer_crud.controller.js
+++ src/controller/admin_controller/customer_crud.controller.js
@@ -372,149 +372,296 @@ exports.getAllDocumentDetailById = async(req, res) =>{
 
   
   // API for count for chat, video and call
-exports.totalCountForCustomer = async(req, res)=>{
+// exports.totalCountForCustomer = async(req, res)=>{
+//   const id = req.query.id;
+//     try {
+//       if(!id){
+//         return res.status(200).json({
+//           status: false,
+//           message:"Please provide valid id",
+//         });
+//       }
+//       const find_user = await User.findOne({
+//         where: {
+//           id: id,
+//           deleted_At : null
+//         }
+//       })
+//       if (!find_user) {
+//         return res.status(200).json({
+//           status: false,
+//           message : "User not exist"
+//         })
+//       }
+//       const results = await chat.findAll({
+//         attributes: ["from_user_id", "to_user_id"],
+//         where: {
+//           [Sequelize.Op.or]: [{ from_user_id: id }, { to_user_id: id }],
+//         },
+//         // include: [{
+//         //   model: User,
+//         //   as: "User",
+//         //   where: { deleted_At: null }
+//         // }],
+//         group:  ["from_user_id", "to_user_id"],
+//       });
+//       console.log(results)
+//       let uniqueChatsCounts;
+//       if (results.length > 0) {
+//         const chats = {};
+//         results.forEach((row) => {
+//           const { from_user_id, to_user_id } = row;
+//           addChat(from_user_id, to_user_id);
+//           addChat(to_user_id, from_user_id);
+//         });
+  
+//         function countUniqueChats() {
+//           const uniqueChats = {};
+//           if (chats[id]) {
+//             chats[id].forEach((receiverId) => {
+//               uniqueChats[receiverId] = (
+//                 uniqueChats[receiverId] || new Set()
+//               ).add(id);
+//             });
+//           }
+//           return uniqueChats;
+//         }
+  
+//         function addChat(userId, counterpartId) {
+//           if (!chats[userId]) {
+//             chats[userId] = [];
+//           }
+//           if (!chats[userId].includes(counterpartId)) {
+//             chats[userId].push(counterpartId);
+//           }
+//         }
+        
+//         uniqueChatsCounts = countUniqueChats();
+//         console.log(uniqueChatsCounts);
+        
+//         // return res.send({
+//         //   status: true,
+//         //   message: "Get Data Successfully",
+//         //   chat_count: Object.keys(uniqueChatsCounts).length,
+//         // });
+//       }
+//         const getCall = await video.findAndCountAll({
+//           // where : {
+//           //   UserId: id
+//           // }
+//           where: {
+//             [Sequelize.Op.or]: [
+//               {
+//                 UserId: id,
+//               },
+//               {
+//                 expert_id: id,
+//               },
+//             ],
+//           },
+//         })
+//       const dataForCall = await call.findAndCountAll({
+//         // where : {
+//         //   UserId: id
+//         // }
+//         where: {
+//           [Sequelize.Op.or]: [
+//             {
+//               UserId: id,
+//             },
+//             {
+//               expert_id: id,
+//             },
+//           ],
+//         },
+//       })
+
+//  // Calculate sum of all earnings (transaction_amount) for video calls
+//  const sum_of_video_callearning = await TransactionHistory.sum('transaction_amount', {
+//   where: {
+//     deduct_type: "video_call",
+//     UserId: id
+//   }
+// });
+
+// // Calculate sum of all earnings (transaction_amount) for Audio calls
+// const sum_of_audio_callearning = await TransactionHistory.sum('transaction_amount', {
+// where: {
+// deduct_type: "audio_call",
+// UserId: id
+// }
+// });
+
+// // Calculate sum of all earnings (transaction_amount) for Chats
+// const sum_of_chat_earning = await TransactionHistory.sum('transaction_amount', {
+//   where: {
+//   deduct_type: "chat",
+//   UserId: id
+//   }
+//   });
+
+//     return res.status(200).json({
+//       status : true,
+//       message : "Showing total_count for total_call ,total_video and total_chat",
+//       call: dataForCall.count || 0,
+//       video: getCall.count || 0,
+//       chat_count: Object.keys(uniqueChatsCounts).length || 0,
+//       Expert_total_video_callearning :sum_of_video_callearning,
+//       Expert_total_audio_callearning :sum_of_audio_callearning,
+//       Expert_total_chat_earning : sum_of_chat_earning || 0
+
+//     })
+//   }catch(error){
+//         return res.status(500).json({
+//           status : false,
+//           message : error.message
+//         })
+//       }
+// }
+
+
+
+exports.totalCountForCustomer = async (req, res) => {
   const id = req.query.id;
-    try {
-      if(!id){
-        return res.status(200).json({
-          status: false,
-          message:"Please provide valid id",
-        });
-      }
-      const find_user = await User.findOne({
-        where: {
-          id: id, 
-          deleted_At : null
-        }
-      })
-      if (!find_user) {
-        return res.status(200).json({
-          status: false,
-          message : "User not exist"
-        })
-      }
-      const results = await chat.findAll({
-        attributes: ["from_user_id", "to_user_id"],
-        where: {
-          [Sequelize.Op.or]: [{ from_user_id: id }, { to_user_id: id }],
-        },
-        include: [{
+  try {
+    if (!id) {
+      return res.status(200).json({
+        status: false,
+        message: "Please provide valid id",
+      });
+    }
+
+    // Check if user exists
+    const find_user = await User.findOne({
+      where: {
+        id: id,
+        deleted_At: null,
+      },
+    });
+
+    if (!find_user) {
+      return res.status(200).json({
+        status: false,
+        message: "User not exist",
+      });
+    }
+
+    // Find all unique chats involving the user
+    const results = await chat.findAll({
+      attributes: ["from_user_id", "to_user_id"],
+      where: {
+        [Sequelize.Op.or]: [{ from_user_id: id }, { to_user_id: id }],
+      },
+      include: [
+        {
           model: User,
           as: "User",
-          where: { deleted_At: null }
-        }],
-        group:  ["from_user_id", "to_user_id", "User.id" ],
-      });
-      console.log(results)
-      let uniqueChatsCounts;
-      if (results.length > 0) {
-        const chats = {};
-        results.forEach((row) => {
-          const { from_user_id, to_user_id } = row;
-          addChat(from_user_id, to_user_id);
-          addChat(to_user_id, from_user_id);
-        });
-  
-        function countUniqueChats() {
-          const uniqueChats = {};
-          if (chats[id]) {
-            chats[id].forEach((receiverId) => {
-              uniqueChats[receiverId] = (
-                uniqueChats[receiverId] || new Set()
-              ).add(id);
-            });
-          }
-          return uniqueChats;
-        }
-  
-        function addChat(userId, counterpartId) {
-          if (!chats[userId]) {
-            chats[userId] = [];
-          }
-          if (!chats[userId].includes(counterpartId)) {
-            chats[userId].push(counterpartId);
-          }
-        }
-        
-        uniqueChatsCounts = countUniqueChats();
-        console.log(uniqueChatsCounts);
-        
-        // return res.send({
-        //   status: true,
-        //   message: "Get Data Successfully",
-        //   chat_count: Object.keys(uniqueChatsCounts).length,
-        // });
-      }
-        const getCall = await video.findAndCountAll({
-          // where : {
-          //   UserId: id
-          // }
-          where: {
-            [Sequelize.Op.or]: [
-              {
-                UserId: id,
-              },
-              {
-                expert_id: id,
-              },
-            ],
-          },
-        })
-      const dataForCall = await call.findAndCountAll({
-        // where : {
-        //   UserId: id
-        // }
-        where: {
-          [Sequelize.Op.or]: [
-            {
-              UserId: id,
-            },
-            {
-              expert_id: id,
-            },
-          ],
+          where: { deleted_At: null },
         },
-      })
+      ],
+      group: ["from_user_id", "to_user_id"],
+    });
 
- // Calculate sum of all earnings (transaction_amount) for video calls
- const sum_of_video_callearning = await TransactionHistory.sum('transaction_amount', {
-  where: {
-    deduct_type: "video_call",
-    UserId: id
-  }
-});
+    let uniqueChatsCounts = 0;
 
-// Calculate sum of all earnings (transaction_amount) for Audio calls
-const sum_of_audio_callearning = await TransactionHistory.sum('transaction_amount', {
-where: {
-deduct_type: "audio_call",
-UserId: id
-}
-});
+    if (results.length > 0) {
+      const chats = {};
 
-// Calculate sum of all earnings (transaction_amount) for Chats
-const sum_of_chat_earning = await TransactionHistory.sum('transaction_amount', {
-  where: {
-  deduct_type: "chat",
-  UserId: id
-  }
-  });
+      // Function to count unique chats
+      function countUniqueChats() {
+        const uniqueChats = {};
+        if (chats[id]) {
+          chats[id].forEach((receiverId) => {
+            uniqueChats[receiverId] = (
+              uniqueChats[receiverId] || new Set()
+            ).add(id);
+          });
+        }
+        return uniqueChats;
+      }
+
+      // Function to add chat
+      function addChat(userId, counterpartId) {
+        if (!chats[userId]) {
+          chats[userId] = [];
+        }
+        if (!chats[userId].includes(counterpartId)) {
+          chats[userId].push(counterpartId);
+        }
+      }
+
+      results.forEach((row) => {
+        const { from_user_id, to_user_id } = row;
+        addChat(from_user_id, to_user_id);
+        addChat(to_user_id, from_user_id);
+      });
+
+      const uniqueChats = countUniqueChats();
+      uniqueChatsCounts = Object.keys(uniqueChats).length;
+    }
+
+    // Count video calls
+    const getCall = await video.findAndCountAll({
+      where: {
+        [Sequelize.Op.or]: [{ UserId: id }, { expert_id: id }],
+      },
+    });
+
+    // Count audio calls
+    const dataForCall = await call.findAndCountAll({
+      where: {
+        [Sequelize.Op.or]: [{ UserId: id }, { expert_id: id }],
+      },
+    });
+
+    // Calculate sum of all earnings (transaction_amount) for video calls
+    const sum_of_video_callearning = await TransactionHistory.sum(
+      "transaction_amount",
+      {
+        where: {
+          deduct_type: "video_call",
+          UserId: id,
+        },
+      }
+    );
+
+    // Calculate sum of all earnings (transaction_amount) for Audio calls
+    const sum_of_audio_callearning = await TransactionHistory.sum(
+      "transaction_amount",
+      {
+        where: {
+          deduct_type: "audio_call",
+          UserId: id,
+        },
+      }
+    );
+
+    // Calculate sum of all earnings (transaction_amount) for Chats
+    const sum_of_chat_earning = await TransactionHistory.sum(
+      "transaction_amount",
+      {
+        where: {
+          deduct_type: "chat",
+          UserId: id,
+        },
+      }
+    );
 
     return res.status(200).json({
-      status : true,
-      message : "Showing total_count for total_call ,total_video and total_chat",
+      status: true,
+      message:
+        "Showing total_count for total_call, total_video, and total_chat",
       call: dataForCall.count || 0,
       video: getCall.count || 0,
-      chat_count: Object.keys(uniqueChatsCounts).length || 0,
-      Expert_total_video_callearning :sum_of_video_callearning,
-      Expert_total_audio_callearning :sum_of_audio_callearning,
-      Expert_total_chat_earning : sum_of_chat_earning || 0
-
-    })
-  }catch(error){
-        return res.status(500).json({
-          status : false,
-          message : error.message
-        })
-      }
-}
+      chat_count: uniqueChatsCounts || 0,
+      Expert_total_video_callearning: sum_of_video_callearning,
+      Expert_total_audio_callearning: sum_of_audio_callearning,
+      Expert_total_chat_earning: sum_of_chat_earning || 0,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: error.message,
+    });
+  }
+};
