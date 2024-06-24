3c4446860df36abd9178041238d5950f927e02ca
akash <akash.bawankar111@gmail.com>
2024-06-24 18:50:39 +0530

    sendgrid

--- src/controller/document_controller/document.controller.js
+++ src/controller/document_controller/document.controller.js
@@ -203,7 +203,7 @@ exports.uploadDocument = async (req, res) => {
   
                     notification: {
                       title: `Document`,
-                      body: `Dear ${receiver.name}, you have received a document sent by ${sender.name}`,
+                      body: ` You have received a document sent by ${sender.name}`,
                       priority: "high"
                     },
                   };
